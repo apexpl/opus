99b57eccc0cb4a5ccb96e3a99cfc0ada8cbbb4b7
Matt Dizak <matt.dizak@gmail.com>
2021-09-29 07:19:26 -0700

    Dev update

---
+++ skel/apex_core/view_index.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types = 1);
+
+namespace Views;
+
+use Apex\Svc\View;
+
+/**
+ * Render the template.
+ */
+class index
+{
+
+    /**
+     * Render
+     */
+    public function render(View $view):void
+    {
+
+    }
+
+}
+
+
